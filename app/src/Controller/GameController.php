d36ee73d23e73ce92d938e8594d8cf4969ef3b27
Cyril David <cdhinho27370@outlook.fr>
2026-04-18 14:35:57 +0200

    feat: suppression d'un jeu avec confirmation JS et token CSRF
    - Ajout de la route /game/{id}/delete en POST uniquement
    - Formulaire de suppression avec token CSRF dans la fiche
    - Confirmation JavaScript avant suppression
    - Message flash vert sur l'accueil après suppression
    - Affichage des messages flash dans index.html.twig

--- app/src/Controller/GameController.php
+++ app/src/Controller/GameController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Game;
 use App\Repository\GameRepository;
 use App\Repository\GenreRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -37,11 +38,24 @@ final class GameController extends AbstractController
         ]);
     }
 
-    #[Route('/game/{id}', name: 'app_game_show')]
+    #[Route('/game/{id}', name: 'app_game_show', requirements: ['id' => '\d+'])]
     public function show (Game $game): Response
     {
         return $this->render('game/show.html.twig', [
             'game' => $game,
         ]);
     }
+
+    #[Route('/game/{id}/delete', name: 'app_game_delete', requirements: ['id' => '\d+'], methods: ['POST'])]
+    public function delete(Request $request, Game $game, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$game->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($game);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Le jeu "' . $game->getTitle() . '" a été supprimé de votre ludothèque.');
+    }
+
+            return $this->redirectToRoute('app_game_index');
+}
 }
